a45df37a0cf06481586d05bc0726c74dfa29bd88
Ryan Roadside <ryan@roadsidemultimedia.com>
2015-11-18 10:56:18 -0800

    add error checking
    Signed-off-by: Ryan Roadside <ryan@roadsidemultimedia.com>

--- simpleJsCopy.js
+++ simpleJsCopy.js
@@ -1,4 +1,4 @@
-/* simpleJsCopy.js v0.2.0 by @ryanpcmcquen */
+/* simpleJsCopy.js v0.2.1 by @ryanpcmcquen */
 
 // Ryan P.C. McQuen | Everett, WA | ryan.q@linux.com
 //
@@ -44,39 +44,48 @@
     if (iPhoneORiPod || iPad || safari) {
       copyBtn.textContent = "Select text";
     }
-    copyBtn.addEventListener('click', function() {
-      // select the text
-      var copyItem = document.querySelector('.text-to-copy');
-      copyItem.select();
-      try {
-        // now that we've selected the text, execute the copy command
-        document.execCommand('copy');
-        if (iPhoneORiPod) {
-          copyBtn.textContent = "Now tap 'Copy'";
-        } else if (iPad) {
-          // the iPad doesn't have the 'Copy' box pop up,
-          // you have to tap the text first
-          copyBtn.textContent = "Now tap the text, then 'Copy'";
-        } else if (safari) {
-          // fancy safari detection thanks to: http://stackoverflow.com/a/23522755
-          copyBtn.textContent = "Press Command + C to copy";
-        } else {
-          copyBtn.textContent = "Copied!";
-        }
-      } catch (ignore) {
-        copyBtn.textContent = "Please copy manually";
-      }
-      // this is what selects it on iOS
-      copyItem.focus();
-      if (iPhoneORiPod || iPad) {
-        copyItem.selectionStart = 0;
-        copyItem.selectionEnd = copyItem.textContent.length;
-      } else {
-        copyItem.select();
-      }
+    if (copyBtn) {
+      copyBtn.addEventListener('click', function() {
+        // select the text
+        var copyItem = document.querySelector('.text-to-copy');
+        if (copyItem) {
+          copyItem.select();
+          try {
+            // now that we've selected the text, execute the copy command
+            document.execCommand('copy');
+            if (iPhoneORiPod) {
+              copyBtn.textContent = "Now tap 'Copy'";
+            } else if (iPad) {
+              // the iPad doesn't have the 'Copy' box pop up,
+              // you have to tap the text first
+              copyBtn.textContent = "Now tap the text, then 'Copy'";
+            } else if (safari) {
+              // fancy safari detection thanks to: http://stackoverflow.com/a/23522755
+              copyBtn.textContent = "Press Command + C to copy";
+            } else {
+              copyBtn.textContent = "Copied!";
+            }
+          } catch (ignore) {
+            copyBtn.textContent = "Please copy manually";
+          }
+          // this is what selects it on iOS
+          copyItem.focus();
+          if (iPhoneORiPod || iPad) {
+            copyItem.selectionStart = 0;
+            copyItem.selectionEnd = copyItem.textContent.length;
+          } else {
+            copyItem.select();
+          }
 
-      // disable the button because clicking it again could cause madness
-      copyBtn.disabled = true;
-    });
+          // disable the button because clicking it again could cause madness
+          copyBtn.disabled = true;
+        } else {
+          throw new Error("You don't have a <textarea> with the class: 'text-to-copy'. Please check the simpleJsCopy README.");
+        }
+      });
+    } else {
+      throw new Error("You don't have a <button> with the class: 'js-copy-btn'. Please check the simpleJsCopy README.");
+    }
   });
 }());
+
