22acf15140b42f00201793481e4a366b463a06f5
Ryan P.C. McQuen <ryan.q@linux.com>
2016-11-06 15:45:54 -0800

    Add initial support for multiple elements.
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- simpleJsCopy.js
+++ simpleJsCopy.js
@@ -1,4 +1,4 @@
-/*! simpleJsCopy.js v0.3.1 by ryanpcmcquen */
+/*! simpleJsCopy.js v1.0.0--DEV by ryanpcmcquen */
 // Ryan P.C. McQuen | Everett, WA | ryan.q@linux.com
 //
 // This program is free software: you can redistribute it and/or modify
@@ -22,38 +22,58 @@
 /*jslint browser:true, white:true, single: true*/
 (function () {
   'use strict';
-  // A simple copy button:
-  // - Copies on awesome browsers/devices.
-  // - Selects text on underwhelming mobile devices.
-  // - The button instructs the user if necessary.
-  var simpleJsCopy = function () {
-    var copyBtn = document.querySelector('.js-copy-btn');
+
+  var texts = Array.prototype.slice.call(document.querySelectorAll(
+    '[class^=text-to-copy]'));
+  var buttons = Array.prototype.slice.call(document.querySelectorAll(
+    '[class^=js-copy-btn]'));
+
+  var sets = {};
+
+  sets.texts = texts.map(function (text) {
+    return text.className.match('text-to-copy').input.replace(
+      /text-to-copy/, '');
+  }).sort();
+
+
+  sets.buttons = buttons.map(function (button) {
+    return button.className.match('js-copy-btn').input.replace(
+      /js-copy-btn/, '');
+  }).sort();
+
+  var matches = Object.keys(sets.texts).map(function (ignore, index) {
+    return sets.texts[index].match(sets.buttons[index]);
+  });
+
+  var throwErr = function (err) {
+    throw new Error(err);
+  };
+  var iPhoneORiPod = false;
+  var iPad = false;
+  var oldSafari = false;
+  var navAgent = navigator.userAgent;
+  if (
+    (/^((?!chrome).)*safari/i).test(navAgent)
+    // ^ Fancy safari detection thanks to: https://stackoverflow.com/a/23522755
+    &&
+    !(/^((?!chrome).)*[0-9][0-9](\.[0-9][0-9]?)?\ssafari/i).test(navAgent)
+    // ^ Even fancier Safari < 10 detection thanks to regex.  :^)
+  ) {
+    oldSafari = true;
+  }
+  // We need to test for older Safari and the device,
+  // because of quirky awesomeness.
+  if (navAgent.match(/iPhone|iPod/i)) {
+    iPhoneORiPod = true;
+  } else if (navAgent.match(/iPad/i)) {
+    iPad = true;
+  }
+  var simpleJsCopy = function (btn, text) {
+    var copyBtn = document.querySelector(btn);
+
     var setCopyBtnText = function (textToSet) {
       copyBtn.textContent = textToSet;
     };
-    var throwErr = function (err) {
-      throw new Error(err);
-    };
-    var iPhoneORiPod = false;
-    var iPad = false;
-    var oldSafari = false;
-    var navAgent = navigator.userAgent;
-    if (
-      (/^((?!chrome).)*safari/i).test(navAgent)
-      // ^ Fancy safari detection thanks to: https://stackoverflow.com/a/23522755
-      &&
-      !(/^((?!chrome).)*[0-9][0-9](\.[0-9][0-9]?)?\ssafari/i).test(navAgent)
-      // ^ Even fancier Safari < 10 detection thanks to regex.  :^)
-    ) {
-      oldSafari = true;
-    }
-    // We need to test for older Safari and the device,
-    // because of quirky awesomeness.
-    if (navAgent.match(/iPhone|iPod/i)) {
-      iPhoneORiPod = true;
-    } else if (navAgent.match(/iPad/i)) {
-      iPad = true;
-    }
     if (iPhoneORiPod || iPad) {
       if (oldSafari) {
         setCopyBtnText("Select text");
@@ -64,7 +84,7 @@
         // Clone the text-to-copy node so that we can
         // create a hidden textarea, with its text value.
         // Thanks to @LeaVerou for the idea.
-        var originalCopyItem = document.querySelector('.text-to-copy');
+        var originalCopyItem = document.querySelector(text);
         var dollyTheSheep = originalCopyItem.cloneNode(true);
         var copyItem = document.createElement('textarea');
         copyItem.style.opacity = 0;
@@ -73,8 +93,7 @@
         // get assigned to the textarea we made.
         copyItem.value = dollyTheSheep.value || dollyTheSheep.textContent;
         document.body.appendChild(copyItem);
-        //console.log(copyItem.value);
-        if (originalCopyItem) {
+        if (copyItem) {
           // Select the text:
           copyItem.focus();
           copyItem.selectionStart = 0;
@@ -117,5 +136,11 @@
       );
     }
   };
-  window.addEventListener('DOMContentLoaded', simpleJsCopy);
+
+  window.addEventListener('DOMContentLoaded', function () {
+    matches.map(function (i) {
+      simpleJsCopy('.js-copy-btn' + i, '.text-to-copy' + i);
+    });
+  });
+
 }());
