3b8a20c1cb5be3ba2b60a67ca4fcf7b3f5d4c9ac
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-20 22:22:56 -0700

    Remove license header.

--- cheval.js
+++ cheval.js
@@ -1,24 +1,7 @@
 // @license magnet:?xt=urn:btih:3877d6d54b3accd4bc32f8a48bf32ebc0901502a&dn=mpl-2.0.txt MPL-v2
 /*! cheval v1.4.0 by ryanpcmcquen */
 // Ryan P. C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version, with the following exception:
-// the text of the GPL license may be omitted.
-//
-// This program is distributed in the hope that it will be useful, but
-// without any warranty; without even the implied warranty of
-// merchantability or fitness for a particular purpose. Compiling,
-// interpreting, executing or merely reading the text of the program
-// may result in lapses of consciousness and/or very being, up to and
-// including the end of all existence and the Universe as we know it.
-// See the GNU General Public License for more details.
-//
-// You may have received a copy of the GNU General Public License along
-// with this program (most likely, a file named COPYING).  If not, see
-// <https://www.gnu.org/licenses/>.
+
 /*global module, window*/
 /*jslint browser: true*/
 (function () {
